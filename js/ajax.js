2a1a6ba9956d54228cf435eba4aaaa29324673c1
Alexander Velichko <alvi@mail.ua>
2019-02-24 20:49:13 +0200

    feat: added json and popup

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,5 @@
 (function () {
-
+    let errorPopup = document.getElementById('error');
     let burgerForm = document.getElementById('order-form');
     burgerForm.addEventListener('submit', function (e) {
         e.preventDefault();
@@ -7,8 +7,18 @@
         fetch('./order.php', {
             method: 'POST',
             body: formData
-        }).then((res) => res.text())
-            .then((text) => console.log(text))
+        }).then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.error) {
+                    errorPopup.querySelector('.error-message').innerHTML = data.error;
+                    $.fancybox('#error');
+                } else if (data.success) {
+                    $.fancybox('#success');
+                    burgerForm.reset();
+
+                }
+            })
             .catch((error) => console.error(error));
     });
 
