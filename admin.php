41c75c670cb0bf5dc8bbada1f9cb7f62f35f981b
Alexander Velichko <alvi@mail.ua>
2019-03-09 18:58:21 +0200

    feat: added twig

--- admin.php
+++ admin.php
@@ -1,10 +1,10 @@
 <?php
+require './vendor/autoload.php';
 require_once './src/function.php';
-?>
 
-<h1> Админ панель</h1>
+$loader = new \Twig\Loader\FilesystemLoader('Views');
+$twig = new \Twig\Environment($loader);
 
-<?php
 $users = 'SELECT * FROM users';
 $db = connect();
 try {
@@ -13,33 +13,7 @@ try {
 } catch (PDOException $e) {
     errorMessage($e->getMessage());
 }
-$data = $query->fetchAll(PDO::FETCH_ASSOC);
-?>
-<h2>Users</h2>
-<table>
-    <thead>
-    <tr>
-        <th>id</th>
-        <th>email</th>
-        <th>name</th>
-        <th>phone</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($data as $user) {
-        echo '<tr>';
-        echo '<td>' . $user['id'] . '</td>';
-        echo '<td>' . $user['email'] . '</td>';
-        echo '<td>' . $user['name'] . '</td>';
-        echo '<td>' . $user['phone'] . '</td>';
-        echo '</tr>';
-    }
-    ?>
-    </tbody>
-</table>
-<?php
-$db = connect();
+$usersData = $query->fetchAll(PDO::FETCH_ASSOC);
 
 $orders = 'SELECT * FROM orders';
 try {
@@ -48,32 +22,6 @@ try {
 } catch (PDOException $e) {
     errorMessage($e->getMessage());
 }
-$data = $query->fetchAll(PDO::FETCH_ASSOC);
-?>
-<h2>Orders</h2>
-<table>
-    <thead>
-    <tr>
-        <th>id</th>
-        <th>user_id</th>
-        <th>address</th>
-        <th>comment</th>
-        <th>card</th>
-        <th>no_call</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
-    foreach ($data as $order) {
-        echo '<tr>';
-        echo '<td>' . $order['id'] . '</td>';
-        echo '<td>' . $order['user_id'] . '</td>';
-        echo '<td>' . $order['address'] . '</td>';
-        echo '<td>' . $order['comment'] . '</td>';
-        echo '<td>' . $order['card'] . '</td>';
-        echo '<td>' . $order['no_call'] . '</td>';
-        echo '</tr>';
-    }
-    ?>
-    </tbody>
-</table>
+$ordersData = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo $twig->render('Dashboard.twig', ['usersData' => $usersData, 'ordersData' => $ordersData]);
